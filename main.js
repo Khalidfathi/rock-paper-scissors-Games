1c5044a530cddbf706607e8105ffd5344e9959ee
NoorFares <92647638+NoorFares@users.noreply.github.com>
2021-12-19 09:41:13 +0200

    Add  section Game

--- main.js
+++ main.js
@@ -1,9 +1,95 @@
+
+"use strict";
+(function() 
+{
+
+
 // Prevent animation on load
 setTimeout(() => {
-    document.body.classList.remove("preload");
-  }, 500);
-  
-  const scoreNumber = document.querySelector(".score__number");
-  let score = 0;
-  
-  
+  document.body.classList.remove("preload");
+}, 500);
+
+setTimeout(() => {
+  document.body.classList.remove("preload");
+}, 500);
+
+// DOM
+
+
+const CHOICES = [
+  {
+    name: "paper",
+    beats: "rock",
+  },
+  {
+    name: "scissors",
+    beats: "paper",
+  },
+  {
+    name: "rock",
+    beats: "scissors",
+  },
+];
+
+
+//document element 
+const choiceButtons = document.querySelectorAll(".choice-btn");
+const gameDiv = document.querySelector(".game");
+
+const scoreNumber = document.querySelector(".score__number");
+let score = 0;
+
+// Game Logic
+choiceButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const choiceName = button.dataset.choice;
+    const choice = CHOICES.find((choice) => choice.name === choiceName);
+    choose(choice);
+  });
+});
+
+function choose(choice) {
+  const aichoice = computerChoose();
+  displayResults([choice, aichoice]);
+  displayWinner([choice, aichoice]);
+}
+
+function computerChoose() {
+  const rand = Math.floor(Math.random() * CHOICES.length);
+  return CHOICES[rand];
+}
+
+
+const choiceButtons = document.querySelectorAll(".choice-btn");
+const gameDiv = document.querySelector(".game");
+
+
+
+
+const scoreNumber = document.querySelector(".score__number");
+let score = 0;
+
+// Game Logic
+choiceButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const choiceName = button.dataset.choice;
+    const choice = CHOICES.find((choice) => choice.name === choiceName);
+    choose(choice);
+  });
+});
+
+function choose(choice) {
+  const aichoice = aiChoose();
+  displayResults([choice, aichoice]);
+  displayWinner([choice, aichoice]);
+}
+
+function aiChoose() {
+  const rand = Math.floor(Math.random() * CHOICES.length);
+  return CHOICES[rand];
+}
+
+
+
+
+});
